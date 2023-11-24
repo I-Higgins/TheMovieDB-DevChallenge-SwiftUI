63105fe25508465dd510ca21a8b472a63582fc36
I-Higgins <lapdog_05_mete@icloud.com>
2023-11-24 17:23:49 +1000

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Isaac Higgins on 24/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
